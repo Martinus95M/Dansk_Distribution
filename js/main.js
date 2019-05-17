3904f01dbdc17299248ecd844596cf1dc7828658
Martinus95M <martinus.mortensen@gmail.com>
2019-05-17 15:15:52 +0200

    Javascript

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
 $( document ).ready(function() {
-  console.log("Hej med dig");
+  console.log("Ready");
     /*Swipe navigation*/
 
     /*Double slider - Sign in/up form*/
 
     /*Count animation from number A to B*/
-    
+    var counter = $('.flipCount'),
+    counterFrom = counter.data('counter-from'),
+    counterTo = counter.data('counter-to');
+  
+  $('.count').each(function() {
+    $(this).prop('Counter', 0).animate({
+      Counter: $(this).text()
+    }, {
+      duration: 3000,
+      easing: 'swing',
+      step: function(now) {
+        $(this).text(Math.round(now * 100) / 100 + "%");
+      }
+    });
+  });
   });
