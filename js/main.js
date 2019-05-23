e25c5009ee69ad374a63d3fdccb259580828d677
Martinus95M <martinus.mortensen@gmail.com>
2019-05-24 01:31:53 +0200

    Booking -  desktop version

--- js/main.js
+++ js/main.js
@@ -3,21 +3,32 @@ $( document ).ready(function() {
     /*Swipe navigation*/
 
     /*Double slider - Sign in/up form*/
+    const signUpButton = document.getElementById('signUp');
+    const signInButton = document.getElementById('signIn');
+    const container = document.getElementById('booking-container');
+    
+    signUpButton.addEventListener('click', () => {
+        container.classList.add('right-panel-active');
+    });
+    
+    signInButton.addEventListener('click', () => {
+        container.classList.remove('right-panel-active');
+    });
 
     /*Count animation from number A to B*/
-  let counter = $('.Count'),
-  counterFrom = counter.data('counter-from'),
-  counterTo = counter.data('counter-to');
+    let counter = $('.Count'),
+    counterFrom = counter.data('counter-from'),
+    counterTo = counter.data('counter-to');
   
-  $('.count').each(function() {
-    $(this).prop('Counter', 0).animate({
-      Counter: $(this).text()
-    }, {
-      duration: 3000,
-      easing: 'linear',
-      step: function(now) {
-        $(this).text(Math.round(now * 100) / 100 + "%");
-      }
+    $('.count').each(function() {
+      $(this).prop('Counter', 0).animate({
+        Counter: $(this).text()
+      }, {
+        duration: 3000,
+        easing: 'linear',
+        step: function(now) {
+          $(this).text(Math.round(now * 100) / 100 + "%");
+        }
+      });
     });
-  });
   });
