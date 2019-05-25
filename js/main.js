220cafa68a54e580a75a86a6b9c75e7eeb7ba4c0
Martinus95M <martinus.mortensen@gmail.com>
2019-05-25 10:28:02 +0200

    javascript

--- js/main.js
+++ js/main.js
@@ -2,19 +2,6 @@ $( document ).ready(function() {
   console.log("Ready");
     /*Swipe navigation*/
 
-    /*Double slider - Sign in/up form*/
-    const signUpButton = document.getElementById('signUp');
-    const signInButton = document.getElementById('signIn');
-    const container = document.getElementById('booking-container');
-    
-    signUpButton.addEventListener('click', () => {
-        container.classList.add('right-panel-active');
-    });
-    
-    signInButton.addEventListener('click', () => {
-        container.classList.remove('right-panel-active');
-    });
-
     /*Count animation from number A to B*/
     let counter = $('.Count'),
     counterFrom = counter.data('counter-from'),
@@ -31,4 +18,19 @@ $( document ).ready(function() {
         }
       });
     });
+
+    /*Double slider - Sign in/up form*/
+    const signUpButton = document.getElementById('signUp');
+    const signInButton = document.getElementById('signIn');
+    const container = document.getElementById('booking-container');
+    
+    signUpButton.addEventListener('click', () => {
+    container.classList.add('right-panel-active');
+    });
+    
+    signInButton.addEventListener('click', () => {
+    container.classList.remove('right-panel-active');
+    });
+
+
   });
